5df118a47182a0ad60071436671ae731c419f5cc
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2024-02-23 16:06:35 +0530

    error middlware to catch internal server errors

--- api/index.js
+++ api/index.js
@@ -2,6 +2,17 @@ import express from "express"
 import mongoose from "mongoose"
 import "dotenv/config"
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+
+app.use((err, req,res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message
+    })
+  })
 
 mongoose.connect(process.env.MONGO_URI_STORE).then(() => {
     console.log("Connected to mongodb!")
@@ -18,3 +29,4 @@ app.listen(PORT, () => {
 })
 
 app.use('/api/user', userRouter)
+app.use('/api/user', authRouter)

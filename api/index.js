f42ed628e3d53f064910311c250e5fe5425dc79c
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2024-02-23 14:09:05 +0530

    Created model,controller & test route

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express"
+import mongoose from "mongoose"
+import "dotenv/config"
+import userRouter from './routes/user.route.js'
+
+mongoose.connect(process.env.MONGO_URI_STORE).then(() => {
+    console.log("Connected to mongodb!")
+}).catch((err) => {
+    console.log(err)
+})
 
 const app = express()
 
@@ -7,3 +16,5 @@ const PORT = 8000
 app.listen(PORT, () => {
     console.log(`Server running on port:${PORT}`)
 })
+
+app.use('/api/user', userRouter)

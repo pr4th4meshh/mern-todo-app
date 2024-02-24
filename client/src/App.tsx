4b44896a9552d899c81816a9e63e12b1b36c26ec
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2024-02-24 17:36:12 +0530

    Created Signin page ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,15 @@
-// import './index.css'
-// import './App.css'
-
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import SignUp from "./pages/SignUp"
 import Home from "./pages/Home"
 import MainLayout from "./layouts/MainLayout"
+import SignIn from "./pages/SignIn"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
         </Route>
---
+++ client/src/pages/SignIn.tsx
@@ -0,0 +1,68 @@
+import { Form, Input, Image } from "antd"
+import ButtonComponent from "../components/ButtonComponent"
+import viteSvg from '../../public/vite.svg'
+
+const onFinish = (values: unknown) => {
+  console.log("Success:", values)
+}
+
+const SignIn = () => {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-white">
+      <div className="flex flex-col items-center justify-center ">
+        <Image
+          className="mb-5"
+          style={{ height: 100 }}
+          preview={false}
+          src={viteSvg}
+          alt="Logo"
+        />
+
+        <Form
+          layout="vertical"
+          name="basic"
+          className="sm:w-[300px] md:w-[400px] border border-gray-300 p-10 rounded-lg "
+          labelCol={{ span: 12 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          // onFinishFailed={onFinishFailed}
+          autoComplete="on"
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ message: "Please input your username!", required: true }]}
+          >
+            <Input
+              placeholder="Enter Username"
+              size="large"
+              className="w-full rounded-sm !focus:bg-white"
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ message: "Please input your password!", required: true }]}
+          >
+            <Input.Password
+              placeholder="Enter Password"
+              size="large"
+              className="password w-full rounded-sm !focus:bg-white"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <ButtonComponent
+              htmlType="submit"
+              className="rounded-sm bg-slate-800 w-full uppercase"
+              name="Sign Up"
+            />
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default SignIn
